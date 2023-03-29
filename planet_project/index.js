c8fe02c90cdcfd605dd207b5e1cca09743c73c90
Ajay <ajayzyan100@gmail.com>
2023-03-29 11:42:07 +0530

    lesson 56 completed

--- planet_project/index.js
+++ planet_project/index.js
@@ -1,26 +1,36 @@
-const {parse} = require('csv-parse');
+const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+const habitablePlanets = [];
 
-//here we use parse to burffer to convert writable 
-// ---> readable stream source to a writable stream destination.a stream that takes in data as suppose to giving u data; 
-// kepler file is the source here,parse function is the destination for our pipe. 
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
 
 fs.createReadStream("kepler_data.csv")
-  .pipe(parse({
-    comment:'#',
-    coloums:true,
-  }))
+  //here we use parse to burffer to convert writable
+  // ---> readable stream source to a writable stream destination.a stream that takes in data as suppose to giving u data;
+  // kepler file is the source here,parse function is the destination for our pipe.
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on("error", (err) => {
-    console.log(err); 
+    console.log(err);
   })
   .on("end", () => {
-    console.log(results);
+    console.log(`${habitablePlanets.length} habitable plantets found!`);
     console.log("done");
   });
-
-// parse();
