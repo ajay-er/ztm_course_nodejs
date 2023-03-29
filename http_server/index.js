4d25983a97287990038f2781e6c94bfc7e07c340
Ajay <ajayzyan100@gmail.com>
2023-03-29 17:09:20 +0530

    lesson 62 passing JSON object

--- http_server/index.js
+++ http_server/index.js
@@ -6,9 +6,12 @@ const PORT = 3000;
 
 const server = http.createServer((req,res)=>{
      res.writeHead(200,{
-        'Content-Type':'text/plain',
+        'Content-Type':'application/json',
      });
-     res.end('Hello!Am genious..heehheehe');
+     res.end(JSON.stringify({
+        id:1,
+        name: 'Sir Isaac Newton'
+    }));
 });
 
 server.listen(PORT,()=>{
