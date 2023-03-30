0539cbaa70df7f509927b16fee41eec6a931287e
Ajay <ajayzyan100@gmail.com>
2023-03-30 10:07:01 +0530

    lesson 75 completed,express and route handling

--- express_project/server.js
+++ express_project/server.js
@@ -4,12 +4,35 @@ const app = express();
 
 const  PORT = 3001 ; 
 
-app.get('/',(req,res)=>{
-    res.send({
-        id:0,
-        name:"Sir Ajay"
-    });
+const friends = [{
+    id:0,
+    name:"Sir Ajay"
+},
+{
+    id:1,
+    name:'Albert Einstein'
+},
+]
+
+app.get('/friends',(req,res)=>{
+    res.json(friends);
 });
+
+
+app.get('/friends/:FriendId',(req,res)=>{
+    const FriendId = Number(req.params.FriendId);
+    const friend = friends[FriendId];
+    if(friend){//if frined undefined --this is not going to work because it is falsy value
+        res.json(friend);
+        // 🔼default is status code 200 if we want to send explicitly then we can use🔽
+        //! res.status(200).json(friend);
+    }else{//? /frineds/22
+        //even it is error good practice returning json object
+        res.status(404).json({
+            error:"Friend does not exist"
+        })
+    }
+})
 
 app.get('/messages',(req,res)=>{
     res.send('<ul><li><h1>Am genious</h1></ul></li>');
