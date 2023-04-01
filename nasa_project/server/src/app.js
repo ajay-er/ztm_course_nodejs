3141acf630e012207faf999253fcc254e86ad8f9
Ajay <ajayzyan100@gmail.com>
2023-04-01 16:22:21 +0530

    lesson 100 cmpltd,runing prjct in one port| server

--- nasa_project/server/src/app.js
+++ nasa_project/server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require('cors');
+const path = require('path');
 
 const planetRouter = require("./routes/planets/planets.router");
 
@@ -9,6 +10,11 @@ app.use(cors({
     origin:'http://localhost:3000'
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'..','build')))
+
 app.use(planetRouter);
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'..','build','index.html'))
+})
 
 module.exports = app;
