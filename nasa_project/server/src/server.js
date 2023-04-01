45faa4ee98d173d226c3cad153595dc8685129fa
Ajay <ajayzyan100@gmail.com>
2023-04-01 14:26:38 +0530

    video 98 cpltd,getPlanets API work perfectly 😊

--- nasa_project/server/src/server.js
+++ nasa_project/server/src/server.js
@@ -2,10 +2,19 @@ const http = require("http");
 
 const app = require("./app");
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 8000;
 
-const server = http.createServer(app);//another way of creating express server which is little bit more flexible || organize
+const server = http.createServer(app); //another way of creating express server which is little bit more flexible || organize
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
+
+startServer();
+//.....
