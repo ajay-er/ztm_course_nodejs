c175ce2f7a97be2020f2c1b33cfd27fc1705746f
Ajay <ajayzyan100@gmail.com>
2023-04-02 19:51:55 +0530

    lesson 108 completed,validate the submitted launch

--- nasa_project/server/src/routes/launches/launches.controller.js
+++ nasa_project/server/src/routes/launches/launches.controller.js
@@ -1,17 +1,32 @@
-const { 
-    getAllLaunches, 
-    addNewLaunch,
- } = require("../../models/launches.model");
+const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
 }
 
-function httpAddNewLaunch(req,res){
-    const launch = req.body;
-    launch.launchDate = new Date(launch.launchDate);
-    addNewLaunch(launch);
-    return res.status(201).json(launch);
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  if (
+    !launch.rocket ||
+    !launch.destination ||
+    !launch.launchDate ||
+    !launch.mission
+  ) {
+    return res.status(400).json({
+        error:"Missing required launch property",
+    });
+  }
+
+  launch.launchDate = new Date(launch.launchDate);
+  if(isNaN(launch.launchDate)){//we can also use --> if(launch.launchDate.toString() === 'Invalid Date'){}
+    return res.status(400).json({
+        error: 'Invalid launch Date',
+    })
+  }
+
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
 }
 
 module.exports = {
