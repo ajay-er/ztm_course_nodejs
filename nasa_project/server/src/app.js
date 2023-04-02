51fd625ad155fb459fe6940acc5d55e31aa04294
Ajay <ajayzyan100@gmail.com>
2023-04-02 17:30:16 +0530

    lesson 105 completed,Layered architecture

--- nasa_project/server/src/app.js
+++ nasa_project/server/src/app.js
@@ -1,25 +1,27 @@
 const express = require("express");
-const cors = require('cors');
-const path = require('path');
-const morgan = require('morgan');
+const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
 
 const planetRouter = require("./routes/planets/planets.router");
 const launchesRouter = require("./routes/launches/launches.router");
 
 const app = express();
 
-app.use(cors({
-    origin:'http://localhost:3000'
-}));
-app.use(morgan('tiny'));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(morgan("tiny"));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'..','build')))
+app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.use(planetRouter);
 app.use(launchesRouter);
-app.get('/*',(req,res) => {
-    res.sendFile(path.join(__dirname,'..','build','index.html'))
-})
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 
 module.exports = app;
