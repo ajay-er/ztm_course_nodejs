cbec93e18f23e610bb3a672db30129f7ad73334d
Ajay <ajayzyan100@gmail.com>
2023-04-02 15:28:36 +0530

    lesson 104,get launches and frnt end url routes

--- nasa_project/server/src/app.js
+++ nasa_project/server/src/app.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 
 const planetRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 const app = express();
 
 app.use(cors({
     origin:'http://localhost:3000'
 }));
+app.use(morgan('tiny'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'..','build')))
 
 app.use(planetRouter);
-app.get('/',(req,res) => {
+app.use(launchesRouter);
+app.get('/*',(req,res) => {
     res.sendFile(path.join(__dirname,'..','build','index.html'))
 })
 
