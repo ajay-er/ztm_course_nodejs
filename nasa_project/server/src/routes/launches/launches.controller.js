f2997a601fae111bfe8de4cf702c302fcb6ea9d8
Ajay <ajayzyan100@gmail.com>
2023-04-03 09:25:51 +0530

    lesson 110 completed,abortmission

--- nasa_project/server/src/routes/launches/launches.controller.js
+++ nasa_project/server/src/routes/launches/launches.controller.js
@@ -1,4 +1,9 @@
-const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
+const {
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
+} = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
@@ -14,22 +19,38 @@ function httpAddNewLaunch(req, res) {
     !launch.mission
   ) {
     return res.status(400).json({
-        error:"Missing required launch property",
+      error: "Missing required launch property",
     });
   }
 
   launch.launchDate = new Date(launch.launchDate);
-  if(isNaN(launch.launchDate)){//we can also use --> if(launch.launchDate.toString() === 'Invalid Date'){}
+  if (isNaN(launch.launchDate)) {
+    //we can also use --> if(launch.launchDate.toString() === 'Invalid Date'){}
     return res.status(400).json({
-        error: 'Invalid launch Date',
-    })
+      error: "Invalid launch Date",
+    });
   }
 
   addNewLaunch(launch);
   return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res) {
+  const launchId = req.params.id;
+  if (!existsLaunchWithId(launchId)) {
+    return res.status(404).json({
+      error: "Launch not found",
+    });
+  }
+
+  if(abortLaunchById(launchId)){
+    
+  }
+  return res.status(200).json(aborted);
+}
+
 module.exports = {
   httpGetAllLaunches,
   httpAddNewLaunch,
+  httpAbortLaunch,
 };
