601b2912620b5a68f5de86466b0b9bdd33ec90d9
Ajay <ajayzyan100@gmail.com>
2023-04-03 14:20:07 +0530

    lesson 114 completed api test --> jest

---
+++ nasa_project/server/src/routes/launches/launches.test.js
@@ -0,0 +1,17 @@
+describe('Test GET /launches',()=>{
+    test('It should respond with 200 sucess',()=>{
+        const response = 200;
+        expect(response).toBe(200);
+    });
+});
+
+describe('Test POST /launches',()=>{
+    test('It should respond with 200 sucess',()=>{
+
+    });
+
+    test('it should catch missing required properties',()=>{})
+        test('it should catch invalid dates',()=>{})
+})
+
+
