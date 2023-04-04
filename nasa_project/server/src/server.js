786980b25fb6e44396097a30300fa0404fe8aa77
Ajay <ajayzyan100@gmail.com>
2023-04-04 15:09:37 +0530

    lesson 142 complted,mongoose ,schema for launch ❤️

--- nasa_project/server/src/server.js
+++ nasa_project/server/src/server.js
@@ -1,15 +1,32 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api:wy8EGqI4QJK3rWjd@nasacluster.bmnatmd.mongodb.net/nasa?retryWrites=true&w=majority";
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app); //another way of creating express server which is little bit more flexible || organize
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
   await loadPlanetsData();
+  await mongoose.connect(MONGO_URL, {
+    //if we dont specify these optional options we get some deprecations warnings in the console
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
